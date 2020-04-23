c3a9cd4531641808003b14fdf2e891499ea915c9
Trevor Le <tle@zephyrsleep.com>
2020-04-23 17:41:03 -0600

    World and country selection implemented with icons and buttons, refresh button added

--- app/src/main/java/com/trevo/covid19app/ui/country/CountryViewModel.kt
+++ app/src/main/java/com/trevo/covid19app/ui/country/CountryViewModel.kt
@@ -1,13 +1,13 @@
-package com.trevo.covid19app.ui.notifications
+package com.trevo.covid19app.ui.country
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class CountryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Country Page"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/trevo/covid19app/ui/world/WorldViewModel.kt
+++ app/src/main/java/com/trevo/covid19app/ui/world/WorldViewModel.kt
@@ -1,13 +1,13 @@
-package com.trevo.covid19app.ui.home
+package com.trevo.covid19app.ui.world
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class WorldViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "World Page"
     }
     val text: LiveData<String> = _text
 }
